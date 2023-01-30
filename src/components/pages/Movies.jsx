5a56654981c4803bb044dbf75126db9d0043e86c
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2023-01-30 17:41:26 +0200

    done

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -11,63 +11,41 @@ import { MovieList, MovieItemLink, MovieItem } from './Movies.styled';
 const Movies = () => {
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
-  const [events, setEvents] = useState(false);
-  const [valueMovie, setValueMovie] = useState('');
-  const [submit, setSubmit] = useState('');
-  console.log(searchParams.get('query'));
-  // let submit  = searchParams.get('query');
-  // console.log(keyword);
-  // if (events === []) {
-  //   return;
-  // }
-  const handleSearchMovie = e => {
-    setValueMovie(e.target.value);
-    console.log('input', e.target.value);
-  };
+  const [movie, setMovie] = useState(null);
+  // const [submit, setSubmit] = useState('');
+
+  const movieName = searchParams.get('query');
+
+  useEffect(() => {
+    if (movieName === '' || movieName === null) return;
+
+    async function getMovie() {
+      const response = await getSearchMovie(movieName);
+      setMovie(response);
+      console.log(response);
+    }
+    getMovie();
+  }, [movieName]);
 
   const handleSubmit = event => {
     event.preventDefault();
-    // const { value } = submit;
-    setSubmit(valueMovie);
     const form = event.currentTarget;
-    // console.log(form.query.value);
-    setSearchParams({ query: form.query.value });
-    // form.reset();
-
-    // console.log('принимаю submit 2', value);
+    setSearchParams({ query: form.elements.query.value });
+    form.reset();
   };
-
-  // console.log('принимаю submit', value);
-  useEffect(() => {
-    // if (events === null) {
-    //   return;
-    // }
-    const conGetMovie = async () => {
-      const responce = await getSearchMovie(submit);
-      console.log(responce);
-      setEvents(responce);
-    };
-    conGetMovie();
-  }, [submit]);
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input
-          value={valueMovie}
-          onChange={handleSearchMovie}
-          type="text"
-          name="query"
-          placeholder="enter movie"
-        />
+        <input type="text" name="query" placeholder="enter movie" />
         <button onSubmit={handleSubmit}>
           <FcCollect /> Search
         </button>
       </form>
 
       <MovieList>
-        {events &&
-          events.results.map(({ id, original_title }) => (
+        {movie &&
+          movie.results.map(({ id, original_title }) => (
             <MovieItem key={id}>
               <MovieItemLink to={`/movies/${id}`} state={{ from: location }}>
                 {original_title}
